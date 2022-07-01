66c2d48da2f67f43449c9c21ac45450c03321981
isaacbrds <isaac.brigido@gmail.com>
2022-07-01 16:19:04 -0300

    Curso de Python - Strings Métodos Mais Comuns

---
+++ metodo_comum_string.py
@@ -0,0 +1,8 @@
+nome_curso = ' Edição de Vídeo '
+print(nome_curso.upper())
+print(nome_curso.lower())
+print(nome_curso.strip())
+print(nome_curso.lstrip())
+print(nome_curso.rstrip())
+print(nome_curso.find('ção'))
+print(nome_curso.replace('Vídeo', 'Música'))
