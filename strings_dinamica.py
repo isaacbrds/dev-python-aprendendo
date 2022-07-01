38481b8979c009085905a3f2798e36f69099e633
isaacbrds <isaac.brigido@gmail.com>
2022-07-01 16:06:35 -0300

    Curso de Python - Strings Dinâmicas

---
+++ strings_dinamica.py
@@ -0,0 +1,13 @@
+nome = 'Carol'
+hobby = 'ouvir musica'
+
+print(f'Olá sou a {nome} e gosto de {hobby}')
+
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletrô'
+t='te'
+
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
