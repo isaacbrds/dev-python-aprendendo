34daa420504032251c4ad17d1d7c5720ffada461
isaacbrds <isaac.brigido@gmail.com>
2022-07-02 11:08:26 -0300

    Curso de Python - Random Desafio 01

---
+++ desafio01_random.py
@@ -0,0 +1,10 @@
+import random
+
+moeda = ['cara', 'coroa']
+
+print(random.choice(moeda))
+
+nomes = ['Jõao', 'Judas', 'Pedro', 'José', 'Jacó']
+print(random.choice(nomes))
+
+print(random.randint(10,100))
